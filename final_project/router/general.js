65b6de5c81ccd732500d94acc215b45726531c15
aviral <avi8a3@gmail.com>
2026-07-08 04:03:33 -0400

    Complete Task 10 - Fetch book list asynchronously using Async/Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,17 +26,27 @@ public_users.get('/', function (req, res) {
   });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-  
-    if (book) {
-      return res.status(200).send(JSON.stringify(book, null, 4));
-    } else {
-      return res.status(404).json({ message: "Book not found" });
-    }
-  });
-  
+const axios = require('axios'); 
+
+public_users.get('/', async function (req, res) {
+  try {
+    const fetchBooks = () => {
+      return new Promise((resolve, reject) => {
+        if (books) {
+          resolve(books);
+        } else {
+          reject("Books data not found");
+        }
+      });
+    };
+
+    const availableBooks = await fetchBooks();
+    
+    return res.status(200).json(availableBooks);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching book list", error: error });
+  }
+});
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const targetAuthor = req.params.author.toLowerCase();
